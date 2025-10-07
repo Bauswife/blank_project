bd3645fb9eaa40891e0d133be1927753f24ad14c
fabianovikkarlsson <fabianovikkarlsson@Fabians-MacBook-Pro.local>
2025-10-07 16:39:11 +0200

    adds fruit

---
+++ src/app/data/fruits.ts
@@ -0,0 +1,75 @@
+export interface Fruit {
+    name: string;
+    price: number;
+}
+
+export const fruits: Fruit[] = [
+    {
+        name: 'Apelsin',
+        price: 10,
+    },
+    {
+        name: 'Bapelsin',
+        price: 20,
+    },
+    {
+        name: 'Citron',
+        price: 14,
+    },
+    {
+        name: 'Dadel',
+        price: 4,
+    },
+    {
+        name: 'Äpple',
+        price: 7,
+    },
+    {
+        name: 'Päron',
+        price: 6,
+    },
+    {
+        name: 'Fikon',
+        price: 3,
+    },
+    {
+        name: 'Guava',
+        price: 12,
+    },
+    {
+        name: 'Hjortron',
+        price: 2,
+    },
+    {
+        name: 'Enbär',
+        price: 1,
+    },
+    {
+        name: 'Jordgubbe',
+        price: 29,
+    },
+    {
+        name: 'Hallon',
+        price: 6,
+    },
+    {
+        name: 'Smultron',
+        price: 9,
+    },
+    {
+        name: 'Stjärnfrukt',
+        price: 40,
+    },
+    {
+        name: 'Durian',
+        price: 32,
+    },
+    {
+        name: 'Nypon',
+        price: 13,
+    },
+    {
+        name: 'Blåbär',
+        price: 19,
+    },
+]
